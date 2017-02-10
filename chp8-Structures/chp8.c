5605669a0ed283159957680d592c924b2750fbae
Heather Kusmierz <heather.kusmierz@gmail.com>
2017-02-09 19:34:19 -0500

    Set up chapter 8

---
+++ chp8-Structures/chp8.c
@@ -0,0 +1,59 @@
+//
+// chp8.c
+// chp-Structures
+//
+// Created by @HKuz on 2/9/17
+// Exercises from Chapter 8: Working with Structures
+//
+
+#include <stdio.h>
+
+
+// Function Prototypes
+
+
+int main(void) {
+  // Execution of exercise functions
+
+  // Exercise 8.02
+  printf("---------------\n");
+  printf("Exercise 8.02\n\n");
+
+  printf("\n\n");
+
+  // Exercise 8.03
+  printf("---------------\n");
+  printf("Exercise 8.03\n\n");
+
+  printf("\n\n");
+
+  // Exercise 8.04
+  printf("---------------\n");
+  printf("Exercise 8.04\n\n");
+
+  printf("\n\n");
+
+  // Exercise 8.05
+  printf("---------------\n");
+  printf("Exercise 8.05\n\n");
+
+  printf("\n\n");
+
+  // Exercise 8.06
+  printf("---------------\n");
+  printf("Exercise 8.06\n\n");
+
+  printf("\n\n");
+
+
+  printf("---------------\n");
+
+  return 0;
+}
+
+
+/*
+  To compile and run with GNU compiler:
+  Compile on terminal with command: gcc <filename.c>
+  Execute on terminal with command: ./a.out
+*/
